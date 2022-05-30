61d10e0a5078b7f4d1cb800ae0a51d1832202d01
Ledacar <ct749063@gmail.com>
2022-05-29 18:46:13 -0600

    mejorar y prueba de deploy

---
+++ templates/gestiondecuentas.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/style/img/Logo-sin texto.png">
+    <title>Login y Register</title>
+    
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/3e26a1e3ce.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="../style/css/allpage.css">
+    <link rel="stylesheet" href="../style/css/cuenta.css">
+</head>
+<body>
+    <div class="retornar">
+        <a href="index.html"><i class="fas fa-angle-left">  Regresar</i></a>
+    </div>
+        <main>
+            <div class="contenedor__todo">
+                <div class="caja__trasera">
+                    <div class="caja__trasera-login">
+                        <h3>¿Ya tienes una cuenta?</h3>
+                        <p>Inicia sesión para entrar en la página de huellitas</p>
+                        <button id="btn__iniciar-sesion">Iniciar Sesión</button>
+                    </div>
+                    <div class="caja__trasera-register">
+                        <h3>¿Aún no tienes una cuenta?</h3>
+                        <p>Regístrate para que puedas iniciar sesión en huellitas</p>
+                        <button id="btn__registrarse">Regístrarse</button>
+                    </div>
+                </div>
+
+                <!--Formulario de Login y registro-->
+                <div class="contenedor__login-register">
+                    <!--Login-->
+                    <form action="" class="formulario__login">
+                        <h2>Iniciar Sesión</h2>
+                        <input type="text" placeholder="Usuario">
+                        <input type="password" placeholder="Contraseña">
+                        <button>Entrar</button>
+                    </form>
+
+                    <!--Register-->
+                    <form action="" class="formulario__register">
+                        <h2>Regístrarse</h2>
+                        <input type="text" placeholder="Nombre completo">
+                        <input type="email" placeholder="Correo Electronico">
+                        <input type="text" placeholder="Usuario">
+                        <input type="password" placeholder="Contraseña">
+                        <button>Regístrarse</button>
+                    </form>
+                </div>
+            </div>
+
+        </main>
+
+        <script src="../style/js/cuentas.js"></script>
+</body>
+</html>
---
+++ templates/masinfo.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Más información</title>
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+</head>
+<body>
+    <div class="scroll-up-btn">
+        <i class="fas fa-angle-up"></i>
+    </div>
+    <nav class="navbarr">
+        <div class="max-width">
+            <div class="logo">
+              <a>Huelli<span>tas</span></a>           
+            </div>
+            <ul class="menu">
+                <li><a href="#homes" class="menu-btn">Inicio</a></li>
+                <li><a href="#sobreMi" class="menu-btn">Acerca de nosotros</a></li>
+                <li><a href="#expe" class="menu-btn">Catalogo</a></li>
+                <li><a href="#skill" class="menu-btn">Agendar cita</a></li>
+                <li><a href="#work" class="menu-btn">Contactanos</a></li>
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </nav>
+</body>
+</html>
