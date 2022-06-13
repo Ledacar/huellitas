91537b637071a7a11bce0bd0196a0d03d42a34e9
Ledacar <ct749063@gmail.com>
2022-06-12 18:42:33 -0600

    Prueba de recaphat3 y validacion

--- style/js/alerts.js
+++ style/js/alerts.js
@@ -182,6 +182,7 @@ function validarFechaMenorActual(date){
     var motivo = document.getElementById('motivo').value = '';
     var fecha = document.getElementById('fecha').value = '';
     }
+    document.getElementById("aa").submit();
   }
 
     const veri_correo = (event) => {
@@ -217,6 +218,7 @@ function validarFechaMenorActual(date){
       var sujeto = document.getElementById('sujeto').value = '';
       var mensaje = document.getElementById('mensaje').value = '';
       }
+      document.getElementById("mens").submit();
   }
 
 
